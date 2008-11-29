569ede9fd35af83198d5c1709915f62fdd1e80ac
Tomaso Tosolini <tomaso.tosolini@gmail.com>
2008-11-29 11:43:29 +0100

    extract_va_options extended to arrays.
    Now clients can call the method getting back arrays instead of
    hashes:
    
      o.method 1,2,3,4
    where after 2 we are in the variable part.
    Returns [3,4].
    Its up to the method to invoke extrat_va_options( x, false ) # want_hash = false

--- lib/support/util.rb
+++ lib/support/util.rb
@@ -6,8 +6,16 @@ module Kernel
     #
     # Returns the hash of the variable args part, if present or empty hash.
     #
-    def extract_va_options x
-        return (x.size.eql?(1) && !x[0].nil? && x[0].is_a?(Hash)) ?  x[0] : {}
+    def extract_va_options x, want_hash = true
+        if x.size.eql?(0) then
+          if want_hash then 
+            return {}
+          else
+            return []
+          end
+        end
+        return x[0] if x[0].is_a?(Hash)
+        return x 
     end
     
 end
