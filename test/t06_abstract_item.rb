b19cd6ab878b14d529f1f0c23cca28da9fbfceb5
Tomaso Tosolini <tomaso.tosolini@gmail.com>
2008-12-03 23:10:17 +0100

    Wrong path unshifting.

--- test/t06_abstract_item.rb
+++ test/t06_abstract_item.rb
@@ -2,8 +2,6 @@
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
  
-$:.unshift File.join(File.dirname(__FILE__),'..','lib')
-
 require 'test/unit'
 
 require 'st_html/ruing/ruing'
