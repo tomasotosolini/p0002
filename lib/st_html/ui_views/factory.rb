6229419d5f00862ef20415d15da6d32c1ac2d14e
Tomaso Tosolini <tomaso.tosolini@gmail.com>
2008-12-03 22:53:34 +0100

    Namespace clash for Exception? More inspection required.

--- lib/st_html/ui_views/factory.rb
+++ lib/st_html/ui_views/factory.rb
@@ -1,6 +1,12 @@
 
 require 'st_html_license'
 
+require 'support/util'
+
+require 'st_html/ui_views/types/st_string_view'
+  #
+  # Used for fallback views
+
 require 'rubygems'
 gem 'activesupport'
 
@@ -8,41 +14,36 @@ require 'active_support'
     #
     # Camelization is in our hearts
     
-require 'support/util'
-
+  
 module StHtml
 module UiViews
     
 class Factory
 
     def self.get(type, *options)
-        
+
         opt = extract_va_options(options)
         views_folder = opt.delete(:views_folder) 
 
-        if views_folder
-
+        if views_folder 
             begin
+
                 require( "#{views_folder}/#{type}_view" )
                 klass = "#{type}_view".camelize
                 return eval( klass ).new(opt)
-            rescue Exception => exc
-                return self.get(type, opt) 
-                  #
-                  # Removed the folder option, let's try again with safe paths.
+            rescue ::Exception
+                return get(type, opt) 
+                      #
+                      # Removed the folder option, let's try again with safe paths.
             end
-
         else
 
             begin
-                require( File.dirname(__FILE__) + "/types/st_#{type}_view" )
+                require( File.join( File.dirname(__FILE__), "types","st_#{type}_view" ) )
                 klass = "st_#{type}_view".camelize
                 return eval( klass ).new(opt)
-            rescue Exception => exc
-                require( File.dirname(__FILE__) + "/types/st_string_view" )
-                klass = "st_string_view".camelize
-                v = eval( klass ).new( {:prefix => "(FBACK.VIEW for #{type}) "} )
-                return v
+            rescue ::Exception
+                return StStringView.new(opt.merge({:prefix => "(FBACK.VIEW for #{type}) "}))
             end
 
         end
