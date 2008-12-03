39331d5d60c3aa2fb6090426ec66d7db8da48ec0
Tomaso Tosolini <tomaso.tosolini@gmail.com>
2008-12-03 22:55:10 +0100

    Wrong path unshifting.

--- test/tests.rb
+++ test/tests.rb
@@ -8,8 +8,10 @@ require 'rubygems'
 gem 'ruby-debug'
 require 'ruby-debug' 
 
+$:.unshift File.join(File.dirname(__FILE__),'..','lib')
+
    # basic things
-require 'test/t01_factory'
+require 'test/t01_views_factory'
 require 'test/t02_render'
 require 'test/t03_vararg'
 
@@ -25,3 +27,7 @@ require 'test/layout/t02_flow_layout'
   # items, container
 require 'test/t06_abstract_item'
 require 'test/t07_abstract_container'
+
+
+  # items, container
+require 'test/elements/t01_default_item'
