86ea918c44663092d0ba6af4d7fd74423787a5b3
Tomaso Tosolini <tomaso.tosolini@gmail.com>
2008-12-03 23:06:14 +0100

    Abstract validator reasonbly simpler. Some cleanup.

--- test/t05_abstract_validator.rb
+++ test/t05_abstract_validator.rb
@@ -2,8 +2,6 @@
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
  
-$:.unshift File.join(File.dirname(__FILE__),'..','lib')
-
 require 'test/unit'
 
 require 'st_html/ruing/abstract_validator'
@@ -27,110 +25,7 @@ class T05_AbstractValidator < Test::Unit::TestCase
         assert_not_nil av.options
         assert_equal "a", av.options[:option1]
 
-        assert_nil av.validation_options
-        assert_nil av.send( :final_validation_options ) # sometimes rules are there only to be forced :)
-        
-        av.validate nil, :voption1 => "c" 
-        assert_not_nil av.validation_options
-        assert_equal "c", av.validation_options[:voption1]
-        assert_not_nil av.send( :final_validation_options )
-        assert_equal "c", av.send( :final_validation_options )[:voption1]
-
     end
     
-    def test_validation_options_hierarchy
-
-        #
-        # we expect validee forced render options
-        #
-        x = Validee.new :validate => { :option => "validee option" }, \
-            :force => { :validate => { :option => "validee forced option" } }
-        av = StHtml::Ruing::AbstractValidator.new :option => "validator option"
-
-        av.validate x, :option => "validator validateoption"
-        assert_equal "validee forced option", av.send( :final_validation_options )[:option]
-
-        return 
-        
-        #
-        # we expect validator validateoptions
-        #
-        x = Validee.new :validate => { :option => "validee option" } 
-        av = StHtml::Ruing::AbstractValidator.new :option => "validator option"
-
-        av.validate x, :option => "validator validateoption"
-        assert_equal "validator validateoption", av.send( :final_validation_options )[:option]
-
-        
-        #
-        # we expect validator options
-        #
-        x = Validee.new :validate => { :option => "validee option" } 
-        av = StHtml::Ruing::AbstractValidator.new :option => "validator option"
-
-        av.validate x
-        assert_equal "validator option", av.send( :final_validation_options )[:option]
-
-        
-        #
-        # we expect validee validate options
-        #
-        x = Validee.new :validate => { :option => "validee option" } 
-        av = StHtml::Ruing::AbstractValidator.new 
-
-        av.validate x
-        assert_equal "validee option", av.send( :final_validation_options )[:option]
-    end
-    
-    def test_validator_result
-        
-        #
-        # we expect correct, without block or Proc
-        #
-        x = Validee.new :validate => { :option => "validee option" }, \
-            :force => { :validate => { :option => "validee forced option" } }
-        av = StHtml::Ruing::AbstractValidator.new :option => "validator option"
-        
-        av.validate x, :option => "validator validateoption"
-        assert_equal StHtml::Ruing::AbstractValidator::CORRECT,  av.response
-        
-        #
-        # we expect correct, with block 
-        #
-        x = Validee.new :validate => { :option => "validee option" }, \
-            :force => { :validate => { :option => "validee forced option" } }
-        av = StHtml::Ruing::AbstractValidator.new :option => "validator option"
-        
-        av.validate x, :option => "validator validateoption" do |ctx, vr, vd|
-            vr.response= ctx::CORRECT
-        end
-        assert_equal StHtml::Ruing::AbstractValidator::CORRECT,  av.response
-        
-        #
-        # we expect wrong, with block 
-        #
-        x = Validee.new :validate => { :option => "validee option" }, \
-            :force => { :validate => { :option => "validee forced option" } }
-        av = StHtml::Ruing::AbstractValidator.new :option => "validator option"
-        
-        av.validate x, :option => "validator validateoption" do |ctx, vr, vd|
-            vr.response= ctx::WRONG
-        end
-        assert_equal StHtml::Ruing::AbstractValidator::WRONG,  av.response
-        
-        #
-        # we expect UNKNOWN, with Proc
-        #
-        x = Validee.new :validate => { :option => "validee option" }, \
-            :force => { :validate => { :option => "validee forced option" } }
-        av = StHtml::Ruing::AbstractValidator.new :option => "validator option"
-
-        av.validate x, :option => "validator validateoption", \
-            :proc =>  Proc.new { |ctx, vr, vd| \
-                        vr.response= ctx::UNKNOWN }
-
-        assert_equal StHtml::Ruing::AbstractValidator::UNKNOWN,  av.response
-        
-    end
     
 end
