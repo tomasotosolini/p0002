a5de981acb962a12cf32723b251e71f99d6d7d4c
Tomaso Tosolini <tomaso.tosolini@gmail.com>
2008-12-13 16:41:52 +0100

    Old licensing.

--- lib/st_html_license.rb
+++
@@ -1,9 +0,0 @@
-=begin
-
-This software is distributed under terms of the MIT License
-
-Authors: Tomaso Tosolini, Stefano Salvador
-Contact: tomaso.tosolini@gmail.com, stefano.salvador@gmail.com
-
-
-=end
