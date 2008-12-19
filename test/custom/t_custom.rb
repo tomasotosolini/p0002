0e7663ec1222de89159705c7bb424a9a4300edb8
Tomaso Tosolini <tomaso.tosolini@gmail.com>
2008-12-19 23:09:25 +0100

    Added new tests for substrings covering.
    This tests try to solve the problem of demostrate that a string
    produced by a builder is how we expect:
    <tag1 x1="a" x2="b"><tag2 x1="a" x2="b">Hello World</tag2></tag1>
    may be produced with the attributes in different order but is equivalant
    from the XML point of view. Please read the comments within the code.

--- test/custom/t_custom.rb
+++ test/custom/t_custom.rb
@@ -14,7 +14,7 @@
 # 
  
 
-# This is the test fiel that explains the functionality of this test: find
+# This is the test file that explains the functionality of this test: find
 # whether the string has or not the same not empty components described by the
 # array
 
@@ -37,5 +37,57 @@ class T00_TestCustom < Test::Unit::TestCase
         assert_not_same_substrings(["a", "b", "c", "bc"], "a bc", "Failed testing the assertion.")
 
         assert_same_substrings(["a", "bc", "b", "c"], "  a  bc b c", "Failed testing the assertion.")
+        
+        # We want to  be sure that the output of a builder:
+        # 
+        # %-<tag1 x2="b" x1="a"><tag2 x2="b" x1="a">HelloWorld</tag2></tag1>
+        # that can be also 
+        # %-<tag1 x1="a" x2="b"><tag2 x1="a" x2="b">HelloWorld</tag2></tag1>
+        # or
+        # %-<tag1 x1="a" x2="b"><tag2 x2="b" x1="a">HelloWorld</tag2></tag1>
+        # is "the same" we expect no matter what exactly will be the "real"
+        # output at a single instance of the builder
+        #
+        # From XML point of view and also from ours the 3 strings above are
+        # the same so we propose the foolowing as a method to decide if them are 
+        # similar
+        # 
+        # 1. %-<tag1- is always present
+        # 2. %-x1="a"- in the first piece may also appear as %-x1="a"><tag2-
+        # 3. %-x2="b"- in the first piece may also appear as %-x2="b"><tag2-
+        # and so on.
+        # Ok: [ %-<tag1-, # for the first,
+        #       [ %-x1="a"-, %-x1="a"><tag2- ], # for the second
+        #       [ %-x2="2"-, %-x2="2"><tag2- ], # for the third
+        # not that %-x1="a"- is also in the second piece, but can appear as
+        # %-x1="a">HelloWorld</tag2></tag1>-
+        
+        
+        assert_substrings_digroups_cover( \
+          ["<tag1", 
+            [%-x1="a"-, %-x1="a"><tag2-], 
+            [%-x2="b"-, %-x2="b"><tag2-],
+            [%-x1="a"-, %-x1="a">HelloWorld</tag2></tag1>-], 
+            [%-x2="b"-, %-x2="b">HelloWorld</tag2></tag1>-],
+              ], %-<tag1 x1="a" x2="b"><tag2 x1="a" x2="b">HelloWorld</tag2></tag1>-, "Failed testing the assertion.")
+          
+        assert_substrings_digroups_cover( \
+          ["<tag1", 
+            [%-x1="a"-, %-x1="a"><tag2-], 
+            [%-x2="b"-, %-x2="b"><tag2-],
+            [%-x3="c"-, %-x3="c"><tag2-],
+            [%-x1="a"-, %-x1="a">Hello-], 
+            [%-x2="b"-, %-x2="b">Hello-], 
+            [%-x3="c"-, %-x3="c">Hello-], 
+            %-World</tag2></tag1>-,
+              ], %-<tag1 x1="a" x2="b" x3="c"><tag2 x1="a" x2="b" x3="c">Hello World</tag2></tag1>-, "Failed testing the assertion.")
+          
+        #
+        # The following string list covers but not in a reasonable way, because
+        # we require that some pieces of string are covered by one element xor
+        # another, not by one element or another.
+        #
+        assert_substrings_digroups_dont_cover(["<tag", %-x1="a"-, %-x1="a"/>-, [%-x2="b"-, %-x2="b"/>-]], %-<tag x2="b" x1="a"/>-, "Failed testing the assertion.")
+        
     end
 end
--- test/custom/test_custom.rb
+++ test/custom/test_custom.rb
@@ -32,24 +32,47 @@ require 'test/unit'
  
 class Test::Unit::TestCase
 
+    
+    
     def assert_same_substrings list, str, message=nil
         @l = list
         @s = str
         process
-        assert @l.size.eql?(0) && @s.strip.empty?, message || "Still have components. list=(#{@l.join(',')}), str=(#{@s})"
+        assert @l.size.eql?(0) && @s.strip.empty?, message || "Not same substrings.\n\nlist=(#{@l.join(',')}),\n\nstr=(#{@s})"
     end
     def assert_not_same_substrings list, str, message=nil
         @l = list
         @s = str
         process 
-        assert !(@l.size.eql?(0) && @s.strip.empty?), message || "Same components. list=(#{@l.join(',')}), str=(#{@s})"
+        assert !(@l.size.eql?(0) && @s.strip.empty?), message || "Same substrings.\n\nlist=(#{@l.join(',')}),\n\nstr=(#{@s})"
     end
     def assert_substrings_cover list, str, message=nil
         @l = list
         @s = str
         process
-        assert( (@l.size >= 0) && @s.strip.empty?, message || "Substring don't cover. list=(#{@l.join(',')}), str=(#{@s})")
+        assert( (@l.size >= 0) && @s.strip.empty?, message || "Substring don't cover.\n\nlist=(#{@l.join(',')}),\n\nstr=(#{@s})")
     end
+    def assert_substrings_digroups_cover list, str, message=nil
+        @l = list
+        @s = str
+        process_by_groups
+        
+        # we want that all the single components were used and removed only 
+        # the digroups have one residual value
+        assert( (@l.size >= 0) \
+                && (@l.all? { |x| x.is_a?(Array) && (x.size.eql?(1)) }) \
+                && @s.strip.empty?, message || "Substring don't digroup-cover .\n\nlist=(#{@l.join(',')}),\n\nstr=(#{@s})")
+    end
+    def assert_substrings_digroups_dont_cover list, str, message=nil
+        @l = list
+        @s = str
+        process_by_groups
+        
+        assert( !((@l.size >= 0) \
+                && (@l.all? { |x| x.is_a?(Array) && (x.size.eql?(1)) }) \
+                && @s.strip.empty?), message || "Substring digroup-cover.\n\nlist=(#{@l.join(',')}),\n\nstr=(#{@s})")
+    end
+    
     private
     def process 
         upper = @l.size-1
@@ -60,6 +83,32 @@ class Test::Unit::TestCase
               @l.delete_at(upper - i)
             elsif @s.sub!( /^#{@l[upper - i]}\s{1}/, " ") then 
               @l.delete_at(upper - i)
+            end
+            break if @s.strip.empty?
+        end 
+    end
+    def process_by_groups 
+        upper = @l.size-1
+        for i in 0..upper do
+            if @l[upper - i].is_a?(Array) then
+                for j in 0...(@l[upper - i].size) do 
+                    if @s.sub!(/\s{1}#{@l[upper - i][j]}\s{1}/, "  ") then 
+                      @l[upper - i].delete_at(j)
+                    elsif @s.sub!(/\s{1}#{@l[upper - i][j]}$/, " ") then 
+                      @l[upper - i].delete_at(j)
+                    elsif @s.sub!( /^#{@l[upper - i][j]}\s{1}/, " ") then 
+                      @l[upper - i].delete_at(j)
+                    end
+                end
+                @l.delete_at(upper - i) if @l[upper - i].size.eql?(0)
+            else    
+                if @s.sub!(/\s{1}#{@l[upper - i]}\s{1}/, "  ") then 
+                  @l.delete_at(upper - i)
+                elsif @s.sub!(/\s{1}#{@l[upper - i]}$/, " ") then 
+                  @l.delete_at(upper - i)
+                elsif @s.sub!( /^#{@l[upper - i]}\s{1}/, " ") then 
+                  @l.delete_at(upper - i)
+                end
             end
             break if @s.strip.empty?
         end 
